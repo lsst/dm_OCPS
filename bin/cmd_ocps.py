1e4d9464e5fa6f6b2b274bf9232ea52e88bb007f
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-12-15 23:10:29 -0600

    Add commander script.

---
+++ bin/cmd_ocps.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# This file is part of dm_OCPS
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+import asyncio
+from lsst.ts.salobj import CscCommander
+
+
+class OcpsCscCommander(CscCommander):
+    def __init__(self, *args, **kwargs):
+        super().__init__(name="OCPS", index=False)
+        for command in ("abort", "enterControl", "setValue"):
+            del self.command_dict[command]
+
+
+asyncio.run(OcpsCscCommander.amain(index=False))
